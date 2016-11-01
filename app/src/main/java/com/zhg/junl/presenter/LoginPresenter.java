2d866abe146d549bce9ace46e4bd1d0bf06c9313
junlyuan <junlyuan@jly.com>
2016-11-01 11:34:49 +0800

    对MVP模式的分析，对Presenter、Model、View的含义及用法总结

--- app/src/main/java/com/zhg/junl/presenter/LoginPresenter.java
+++ app/src/main/java/com/zhg/junl/presenter/LoginPresenter.java
@@ -1,38 +1,73 @@
 package com.zhg.junl.presenter;
 
+import android.os.Handler;
+
 import com.zhg.junl.biz.IPersonBiz;
 import com.zhg.junl.biz.LoginRequestCallBack;
 import com.zhg.junl.biz.imp.PersonBizImp;
 import com.zhg.junl.entity.PersonBean;
 import com.zhg.junl.ui.view.ILoginView;
 
-import java.util.logging.Handler;
-
 /**
  * 当前类注释:负责完成登录界面View与Model(IPersonBiz)间的交互
+ * <p/>
+ * Presenter层，作为Model和View之间交互的桥梁，在本例中执行登陆操作，然后去Model业务中执行登陆操作，
+ * 然后将登陆结果信息返回给View层
  */
 public class LoginPresenter {
-    private static final String TAG ="LoginPresenter";
+    private static final String TAG = "LoginPresenter";
     private ILoginView mLoginView;
     private IPersonBiz mPersonBiz;
 
-//    private Handler mHandler=new Handler();
+    private Handler mHandler = new Handler();
 
-    public LoginPresenter(ILoginView view){
+    /**
+     * 构造方法中初始化Model实例IPersonBiz，用来调用登陆的方法
+     * @param view
+     */
+    public LoginPresenter(ILoginView view) {
         mLoginView = view;
         mPersonBiz = new PersonBizImp();
     }
 
-    public void loginSystem(){
+    public void loginSystem() {
         mPersonBiz.login(mLoginView.getUserName(), mLoginView.getPassword(), new LoginRequestCallBack() {
-            @Override
-            public void loginSuccess(PersonBean personBean) {
 
+            /**
+             * 用户业务逻辑实现类方法
+             * 登陆成功的回调方法
+             * @param personBean
+             */
+            @Override
+            public void loginSuccess(final PersonBean personBean) {
+                mHandler.post(new Runnable() {
+                    @Override
+                    public void run() {
+                        //登陆结果返回给View层
+                        mLoginView.showSuccessInfo(personBean);
+                    }
+                });
             }
 
+
+            /**
+             * 用户业务逻辑实现类
+             * 登陆失败回调的方法
+             */
             @Override
             public void loginFailed() {
-
+                /**
+                 * 不使用handler时，直接mLoginView.showFailedInfo();会报以下错
+                 * Can't create handler inside thread that has not called Looper.prepare()
+                 * 第一印象就是去Loop.prepare(),后来发现，其实这还是因为在线程中更新UI导致的，
+                 * http://blog.csdn.net/jason0539/article/details/21196485
+                 */
+                mHandler.post(new Runnable() {
+                    @Override
+                    public void run() {
+                        mLoginView.showFailedInfo();
+                    }
+                });
             }
         });
     }
--- app/src/main/java/com/zhg/junl/test/MVPTestActiviy.java
+++ app/src/main/java/com/zhg/junl/test/MVPTestActiviy.java
@@ -1,9 +1,13 @@
 package com.zhg.junl.test;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 import com.zhg.junl.R;
 import com.zhg.junl.entity.PersonBean;
+import com.zhg.junl.presenter.LoginPresenter;
 import com.zhg.junl.ui.base.BaseActivity;
 import com.zhg.junl.ui.view.ILoginView;
 
@@ -12,29 +16,67 @@ import com.zhg.junl.ui.view.ILoginView;
  */
 public class MVPTestActiviy extends BaseActivity implements ILoginView {
 
+    private LoginPresenter mLoginPresenter;
+    private EditText ed_username;
+    private EditText ed_password;
+    private Button btn_login;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.mvp_test_layout);
+
+        ed_username=(EditText)this.findViewById(R.id.ed_username);
+        ed_password=(EditText)this.findViewById(R.id.ed_password);
+        btn_login=(Button)this.findViewById(R.id.btn_login);
+
+        /**
+         *  初始化LoginPresenter
+         *  this指代是ILoginView，在LoginPresenter的构造方法中，将该接口传递进去
+         *
+         */
+        mLoginPresenter = new LoginPresenter(this);
+
+        btn_login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mLoginPresenter.loginSystem();
+            }
+        });
     }
 
+    /**
+     * 返回用户名信息
+     * @return
+     */
     @Override
     public String getUserName() {
-        return null;
+        return ed_username.getText().toString().trim();
     }
 
+    /**
+     * 返回用户密码信息
+     * @return
+     */
     @Override
     public String getPassword() {
-        return null;
+        return ed_password.getText().toString().trim();
     }
 
+    /**
+     * 登陆成功的回调
+     * @param personBean
+     */
     @Override
     public void showSuccessInfo(PersonBean personBean) {
-
+        showToastMsgLong("登陆成功："+personBean.toString());
     }
 
+    /**
+     * 登陆失败的回调
+     */
     @Override
     public void showFailedInfo() {
-
+        showToastMsgShort("登陆失败");
     }
 }
